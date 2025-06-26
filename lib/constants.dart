6426d4326d857e67d6bced4a00b834650fa40af5
Ayodele Fagbami <ayodele@flanceapp.com>
2025-06-26 23:29:34 +0200

    added text and appbar themes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kLightPurple = Color(0xFFAB9FF2);
+const kWhite = Color(0xFFFFFFFF);
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,7 +1,51 @@
+import 'package:app/constants.dart';
 import 'package:flutter/material.dart';
 
 ThemeData theme() {
   return ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    scaffoldBackgroundColor: kWhite,
+    colorScheme: const ColorScheme.light(),
+    appBarTheme: appBarTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    textTheme: textTheme(),
   );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+      letterSpacing: -0.4,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      letterSpacing: -0.4,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 0,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+      letterSpacing: 0,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+      letterSpacing: 0,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+      letterSpacing: 0,
+    ),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(centerTitle: true, elevation: 0, color: kWhite);
 }
