abb2d26b9303b5fd7799f3c51fe25ad46c0e3ff9
Ayodele Fagbami <ayodele@flanceapp.com>
2025-06-26 23:42:55 +0200

    added onboarding appbar

--- lib/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app/size_config.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -5,6 +6,19 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: getProportionateScreenHeight(34)),
+            child: TextButton(
+              onPressed: () {},
+              child: Text("Skip", style: Theme.of(context).textTheme.bodyLarge),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
