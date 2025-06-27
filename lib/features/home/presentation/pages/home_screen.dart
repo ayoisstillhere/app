7258b293de80d2c43954050b2c57f351d2c3a4e0
Ayodele Fagbami <ayodele@flanceapp.com>
2025-06-28 01:07:54 +0200

    Home Screen skeleton

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,10 +1,165 @@
+import 'package:app/constants.dart';
+import 'package:app/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final dividerColor =
+        MediaQuery.of(context).platformBrightness == Brightness.dark
+        ? kGreyInputFillDark
+        : kGreyInputBorder;
+    final iconColor =
+        MediaQuery.of(context).platformBrightness == Brightness.dark
+        ? kWhite
+        : kBlack;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(getProportionateScreenHeight(100)),
+        child: SafeArea(
+          child: AppBar(
+            leading: Padding(
+              padding: EdgeInsets.only(left: getProportionateScreenWidth(16)),
+              child: Container(
+                height: getProportionateScreenHeight(34),
+                width: getProportionateScreenWidth(34),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            bottom: TabBar(
+              controller: controller,
+              indicatorColor: kLightPurple,
+              dividerColor: dividerColor,
+              labelStyle: Theme.of(
+                context,
+              ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w500),
+              unselectedLabelStyle: Theme.of(
+                context,
+              ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w500),
+              tabs: [
+                Tab(
+                  child: SizedBox(
+                    width: getProportionateScreenWidth(143),
+                    child: Center(child: Text("Recomended")),
+                  ),
+                ),
+                Tab(
+                  child: SizedBox(
+                    width: getProportionateScreenWidth(143),
+                    child: Center(child: Text("Following")),
+                  ),
+                ),
+              ],
+              indicatorSize: TabBarIndicatorSize.label,
+            ),
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(
+                  right: getProportionateScreenWidth(22),
+                ),
+                child: SizedBox(
+                  height: getProportionateScreenHeight(24),
+                  width: getProportionateScreenWidth(24),
+                  child: SvgPicture.asset(
+                    "assets/icons/bell.svg",
+                    colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: [
+          Center(child: Text("Recomended")),
+          Center(child: Text("Following")),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        mini: false,
+        elevation: 0,
+        highlightElevation: 0,
+        backgroundColor: kLightPurple,
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(
+            left: getProportionateScreenWidth(32),
+            right: getProportionateScreenWidth(32),
+            top: getProportionateScreenHeight(10),
+          ),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: dividerColor, width: 1)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SvgPicture.asset(
+                "assets/icons/home.svg",
+                colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+              ),
+              SvgPicture.asset(
+                "assets/icons/search.svg",
+                colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+              ),
+              SvgPicture.asset(
+                "assets/icons/message_icon.svg",
+                colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+              ),
+              Container(
+                height: getProportionateScreenHeight(34),
+                width: getProportionateScreenWidth(34),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
