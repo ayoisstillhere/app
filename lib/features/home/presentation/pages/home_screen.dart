addf1002db4973ec7be6edcd1cd482ea72ca43de
Ayodele Fagbami <ayodele@flanceapp.com>
2025-06-28 01:15:31 +0200

    Clean up

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:app/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../widgets/bottom_nav_bar.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -120,60 +122,9 @@ class _HomeScreenState extends State<HomeScreen>
         backgroundColor: kLightPurple,
         child: Icon(Icons.add),
       ),
-      bottomNavigationBar: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(
-            left: getProportionateScreenWidth(32),
-            right: getProportionateScreenWidth(32),
-            top: getProportionateScreenHeight(10),
-          ),
-          width: double.infinity,
-          decoration: BoxDecoration(
-            border: Border(top: BorderSide(color: dividerColor, width: 1)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              InkWell(
-                onTap: () {},
-                child: SvgPicture.asset(
-                  "assets/icons/home.svg",
-                  colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: SvgPicture.asset(
-                  "assets/icons/search.svg",
-                  colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: SvgPicture.asset(
-                  "assets/icons/message_icon.svg",
-                  colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  height: getProportionateScreenHeight(34),
-                  width: getProportionateScreenWidth(34),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      bottomNavigationBar: BottomNavBar(
+        dividerColor: dividerColor,
+        iconColor: iconColor,
       ),
     );
   }
