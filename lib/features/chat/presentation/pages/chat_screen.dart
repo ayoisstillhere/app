dc571db97505624eafc386ca5fd8c79d623aac9d
Ayodele Fagbami <ayodele@flanceapp.com>
2025-06-30 03:30:11 +0200

    Chat Chips

--- lib/features/chat/presentation/pages/chat_screen.dart
+++ lib/features/chat/presentation/pages/chat_screen.dart
@@ -1,10 +1,204 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class ChatScreen extends StatelessWidget {
+import '../../../../constants.dart';
+import '../../../../size_config.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  String selectedChip = "All";
+  final List<Map<String, dynamic>> filters = [
+    {'label': 'All', 'count': 15},
+    {'label': 'Groups', 'count': 2},
+    {'label': 'Secret', 'count': 2},
+    {'label': 'Archived', 'count': 2},
+    {'label': 'Requests', 'count': 2},
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Chat Screen")));
+    final dividerColor =
+        MediaQuery.of(context).platformBrightness == Brightness.dark
+        ? kGreyInputFillDark
+        : kGreyInputBorder;
+    final iconColor =
+        MediaQuery.of(context).platformBrightness == Brightness.dark
+        ? kWhite
+        : kBlack;
+    final selectedChipColor =
+        MediaQuery.of(context).platformBrightness == Brightness.dark
+        ? kGreyInputFillDark
+        : kLightPurple;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Messages",
+          style: TextStyle(
+            fontSize: getProportionateScreenHeight(24),
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        leading: Container(),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: getProportionateScreenWidth(22)),
+            child: SizedBox(
+              height: getProportionateScreenHeight(24),
+              width: getProportionateScreenWidth(24),
+              child: InkWell(
+                onTap: () {},
+                child: SvgPicture.asset(
+                  "assets/icons/edit.svg",
+                  colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+                  width: getProportionateScreenWidth(24),
+                  height: getProportionateScreenHeight(24),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: getProportionateScreenHeight(17)),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(16),
+              ),
+              child: TextFormField(decoration: _buildChatSearchField(context)),
+            ),
+            SizedBox(height: getProportionateScreenHeight(34)),
+            Padding(
+              padding: EdgeInsets.only(left: getProportionateScreenWidth(17)),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/icons/sliders-horizontal.svg",
+                      colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+                      width: getProportionateScreenWidth(15),
+                      height: getProportionateScreenHeight(15),
+                    ),
+                    SizedBox(width: getProportionateScreenWidth(11)),
+                    ...filters.map((filter) {
+                      final isSelected = selectedChip == filter['label'];
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 12),
+                        child: GestureDetector(
+                          onTap: () =>
+                              setState(() => selectedChip = filter['label']),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              color: isSelected
+                                  ? selectedChipColor
+                                  : Colors.transparent,
+                              borderRadius: BorderRadius.circular(30),
+                              border: Border.all(
+                                color: dividerColor,
+                                width: isSelected ? 0 : 1.5,
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                Text(
+                                  filter['label'],
+                                  style: TextStyle(
+                                    fontSize: getProportionateScreenHeight(12),
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(width: 6),
+                                Text(
+                                  filter['count'].toString(),
+                                  style: TextStyle(
+                                    color: Colors.greenAccent,
+                                    fontSize: getProportionateScreenHeight(12),
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _buildChatSearchField(BuildContext context) {
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      fillColor: MediaQuery.of(context).platformBrightness == Brightness.dark
+          ? kGreySearchInput
+          : null,
+      filled: MediaQuery.of(context).platformBrightness == Brightness.dark,
+      prefixIcon: Padding(
+        padding: EdgeInsets.all(getProportionateScreenHeight(14)),
+        child: SvgPicture.asset(
+          "assets/icons/search.svg",
+          colorFilter: ColorFilter.mode(
+            MediaQuery.of(context).platformBrightness == Brightness.dark
+                ? kGreyDarkInputBorder
+                : kGreyInputBorder,
+            BlendMode.srcIn,
+          ),
+          width: getProportionateScreenWidth(14),
+          height: getProportionateScreenHeight(14),
+        ),
+      ),
+      hintText: "Search",
+    );
   }
 }
