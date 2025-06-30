6d67cac69bd0748426e9be4ec81cd3462487267a
Ayodele Fagbami <ayodele@flanceapp.com>
2025-06-30 17:32:54 +0200

    settings search field

--- lib/features/profile/presentation/pages/settings_screen.dart
+++ lib/features/profile/presentation/pages/settings_screen.dart
@@ -1,10 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../constants.dart';
+import '../../../../size_config.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Settings")));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Settings",
+          style: TextStyle(
+            fontSize: getProportionateScreenHeight(24),
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: false,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: getProportionateScreenHeight(40)),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(16),
+              ),
+              child: TextFormField(
+                decoration: _buildChatSearchFieldDecoration(context),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _buildChatSearchFieldDecoration(BuildContext context) {
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(getProportionateScreenWidth(30)),
+        borderSide: BorderSide(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? kGreySearchInput
+              : kGreyInputBorder,
+        ),
+      ),
+      fillColor: MediaQuery.of(context).platformBrightness == Brightness.dark
+          ? kGreySearchInput
+          : null,
+      filled: MediaQuery.of(context).platformBrightness == Brightness.dark,
+      prefixIcon: Padding(
+        padding: EdgeInsets.all(getProportionateScreenHeight(14)),
+        child: SvgPicture.asset(
+          "assets/icons/search.svg",
+          colorFilter: ColorFilter.mode(
+            MediaQuery.of(context).platformBrightness == Brightness.dark
+                ? kGreyDarkInputBorder
+                : kGreyInputBorder,
+            BlendMode.srcIn,
+          ),
+          width: getProportionateScreenWidth(14),
+          height: getProportionateScreenHeight(14),
+        ),
+      ),
+      hintText: "Search ",
+      hintStyle: TextStyle(
+        fontSize: getProportionateScreenHeight(13),
+        fontWeight: FontWeight.w500,
+        color: kGreyDarkInputBorder,
+      ),
+    );
   }
 }
