80f50658cbbb79f59884abf83d698cb1856a2b14
Ayodele Fagbami <ayodele@flanceapp.com>
2025-07-01 02:19:50 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -21,6 +21,7 @@ const kGreyTimeText = Color(0xFF858585);
 const kGreySearchInput = Color(0xFF1F1F1F);
 const kPrimPurple = Color(0xFF9500FF);
 const kProfileText = Color(0xFF727272);
+const kChatBubble = Color(0xFF373737);
 const kChatBubbleGradient = LinearGradient(
   begin: Alignment.topLeft,
   end: Alignment.bottomRight,
